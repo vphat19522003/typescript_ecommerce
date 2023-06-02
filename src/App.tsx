41f37c5a845a73a3fc397a94a3d7bce5c7425d28
vphat19522003 <19522003@gm.uit.edu.vn>
2023-06-02 09:25:50 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Navbar from "./components/Navbar";
+import { About, Home, Store } from "./pages";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+
+function App() {
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
+}
+
+export default App;
